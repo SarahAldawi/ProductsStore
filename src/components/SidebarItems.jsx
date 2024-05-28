e5cea10b61680fbb462a0b08b492e7876e2f2a32
Sarah Aldawi <140004200+SarahAldawi@users.noreply.github.com>
2024-05-28 15:52:19 +0300

    Update SidebarItems.jsx

--- src/components/SidebarItems.jsx
+++ src/components/SidebarItems.jsx
@@ -6,10 +6,6 @@ function SidebarItems() {
     <div>
       <Categories />
       <Brands />
-      {/* <label htmlFor="points" className="text-xl font-medium">
-        price ($0 to $1000):
-      </label>
-      <input type="range" id="points" name="points" min="0" max="10"></input> */}
     </div>
   );
 }
